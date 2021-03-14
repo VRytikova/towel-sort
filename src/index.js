70d36e981af327062a2948f959201bb4b9416084
RVeronica <akinrv16@gmail.com>
2021-03-15 00:18:41 +0300

    feat: implemet towel-sort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+
+    if(matrix === undefined || matrix.length===0){
+        return [];
+    } else {
+        for(let i=1;i<matrix.length;i++) {
+            if(i%2!==0) {
+                matrix[i] = matrix[i].reverse();
+            }
+        }
+        return [].concat(...matrix);
+    }
 }
